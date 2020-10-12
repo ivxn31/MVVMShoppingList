cc19c176ebd9075a71393dc88c73292829031eb7
ivant <ivant@capmega.com>
2020-10-12 17:52:45 -0500

    Add dependencies, change name layout of MainActivity and create ShoppingActivity

---
+++ app/src/main/java/com/developer/mvvmshoppinglist/ShoppingActivity.kt
@@ -0,0 +1,19 @@
+package com.developer.mvvmshoppinglist
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/* 
+ * Capmega Soluciones Informáticas S.A.S de C.V 
+ * October 2020
+ * Iván Tecpanecatl 
+ * ivant@capmega.com
+ */
+
+class ShoppingActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.shopping_activity)
+    }
+}
